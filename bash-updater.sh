6f6122323d17f60243a7e6fa1fc684067e57d088
Alzy <140841524+LazyAlzy@users.noreply.github.com>
2023-12-25 20:04:46 +0000

    Delete bash-updater.sh

--- bash-updater.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo pacman -Syyu
-yay
-notify-send -a 'System Updater' -i /home/alzy/.local/share/icons/Papirus/64x64/apps/software-store.svg 'Update Completed' 'Restart is recommended'
-paplay /usr/share/sounds/Oxygen-K3B-Finish-Success.ogg
