817dffb264fb3ce13acca36376483ff93ee7de4e
Alzy <140841524+LazyAlzy@users.noreply.github.com>
2023-12-25 21:32:33 +0000

    Update filepull.sh

--- filesync/filepull.sh
+++ filesync/filepull.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 SOURCE=/home/$USER
+DIRECTORY=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-touch ext-drive.txt
-
-DEST=$(<ext-drive.txt)
+DEST=$(<$DIRECTORY/ext-drive.txt)
 
 if [ -z "$DEST" ]; then
     echo "Input complete path to backup location"
     read DEST
-    echo $DEST > ext-drive.txt
+    echo $DEST > $DIRECTORY/ext-drive.txt
 fi
 
 if [ -d $DEST ]; then
@@ -17,14 +16,15 @@ if [ -d $DEST ]; then
     rsync -Pruv $DEST/* $SOURCE/
     rsync -Pruv $DEST/.bashrc $SOURCE/.bashrc
 
-    notify-send -a 'File Synchroniser' 'Synchronisation Completed'
+    notify-send -a 'File Synchroniser' -i /home/alzy/.local/share/icons/Papirus/64x64/apps/synology-cloud-station-drive.svg 'Synchronisation Completed'
+
 else
     echo "Folder not found"
     read -p "Would you like to remove the destination path? [Y/n]" -n 1 -r reply
     
     if [ ${reply^} == "Y" ]; then
         echo -e "\nClearing Saved Destination"
-        echo "" > ext-drive.txt
+        echo "" > $DIRECTORY/ext-drive.txt
         echo "You will be prompted for the destination path upon next run"
     else
         echo ""
