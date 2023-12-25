9a5a5902c01a6e8bc61b25316d88676cc3e04f17
Alzy <140841524+LazyAlzy@users.noreply.github.com>
2023-12-25 20:51:20 +0000

    Update file-push.sh

--- file-sync/file-push.sh
+++ file-sync/file-push.sh
@@ -1,1 +1,38 @@
-push
+#!/bin/bash
+
+SOURCE=/home/$USER
+
+touch ext-drive.txt
+
+DEST=$(<ext-drive.txt)
+
+if [ -z "$DEST" ]; then
+    echo "Input complete path to backup location"
+    read DEST
+    echo $DEST > ext-drive.txt
+fi
+
+if [ -d $DEST ]; then
+    echo -e "\n Pushing Files to Sync Drive/\n=====================================" 
+    rsync -Pruv $SOURCE/Documents $DEST/
+    rsync -Pruv $SOURCE/Downloads $DEST/
+    #rsync -Pruv $SOURCE/Pictures $DEST/
+    #rsync -Pruv $SOURCE/Music $DEST/
+    #rsync -Pruv $SOURCE/Videos $DEST/
+    rsync -Pruv $SOURCE/.keepassxc $DEST/
+    rsync -Pruv $SOURCE/.bashrc $DEST/.bashrc
+    rsync -Pruv $SOURCE/Games/BackUps $DEST/Games/
+
+    notify-send -a 'File Synchroniser' -i /home/alzy/.local/share/icons/Papirus/64x64/apps/synology-cloud-station-drive.svg 'Synchronisation Completed'
+else
+    echo "Folder not found"
+    read -p "Would you like to remove the destination path? [Y/n]" -n 1 -r reply
+    
+    if [ ${reply^} == "Y" ]; then
+        echo -e "\nClearing Saved Destination"
+        echo "" > ext-drive.txt
+        echo "You will be prompted for the destination path upon next run"
+    else
+        echo ""
+    fi
+fi
